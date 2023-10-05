7367a8637d18e05129669fec163def5ebd37e803
nuriamayang17 <nuriamayang@gmail.com>
2023-10-05 17:01:31 +0700

    first init github

---
+++ application/controllers/homepage.php
@@ -0,0 +1,11 @@
+<?php
+class profile extends CI_Controller
+{
+    public function index()
+{
+    echo "<h1>perkenalkan</h1>";
+    echo "nama saya nuria mayangsari
+         saya tinggal di daerah cilebut
+         olah raga yang saya sukai adalah berenang";
+}
+}
---
+++ application/views/profile.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html
+   <head>
+   <title>Biodata Diri</title>
+    <link rel="stylesheet" type="text/css" href="contoh.css" />
+    <head>
+    <style>
+       .container {
+        max-width:800px;
+        margin: auto;
+        padding: 20px;
+        background: ;
+        color:red;
+        font-family: arial, sans-serif;
+}
+
+h1 {
+    text-align: center;
+    margin-top: 0;
+}
+img{
+    display: blok;
+    margin: auto;
+    max-width:100px;
+    height: auto;
+    border-radius:50%;
+}
+       .info {
+        margin-top:20px;
+       }
+
+    h2 {
+        font-size: 20px;
+        margin-top: 20px;
+    }
+
+    p {
+        margin: 0;
+    }
+    ul,
+    ol {
+        margin: 0;
+        padding: 0 0 20px;
+    }
+
+    li{
+        margin: spx 0;
+    }
+</style>
+<body>
+    <div class="container">
+        <h1>HOMEPAGE MAYAANGG</h1>
+        <img src="pandaa.png"
+        <div class="info">
+            <ul>
+            <h2>Data Pribadi</h2>
+
+             <li><p>Nama Lengkap  : Nuria Mayangsari</p>
+             <li><p>Tempat, Tanggal Lahir : Sragen,17 Januari 2004</p>
+             <li><p>Alamat   : perumahan rs pmi blok b2 no 7</p>
+             <li><p> Email     : nuriamayang@gmail.com</p>
+             <li><p> Whatsapp    : 089531572357</p>
+
+            <h2>pendidikan</h2>
+            <ul>
+            <ul>
+                <li>SMK Negeri 3 Kota Bogor (2019-2022) </li>
+                <li>Universitas Bina Sarana Informatika </li>
+            <ul>
+            <h2>pengalaman kerja</h2>
+            <ul>
+                <li> PT abc, programmer (2022-2023)</li>
+                <li> PT xyz, web developer (2023-sekarang)</li>
+      </ol>
+     </div>
+   </div>
+  </body>
+</html>
+</head>
